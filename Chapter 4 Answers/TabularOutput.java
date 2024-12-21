63204d2057804d447b851f176eeb0c95f57151e0
Quayyum-a <quayyumariyo@gmail.com>
2024-12-21 18:13:22 +0100

    Update TabularOutput.java

--- Chapter 4 Answers/TabularOutput.java
+++ Chapter 4 Answers/TabularOutput.java
@@ -1,9 +1,9 @@
 public class TabularOutput {
     public static void main(String[] args) {
-        // Print the header
+        
         System.out.println("N\tN^2\tN^3\tN^4");
 
-        // Loop to generate table values
+        
         for (int n = 1; n <= 5; n++) {
             System.out.println(n + "\t" + (n * n) + "\t" + (n * n * n) + "\t" + (n * n * n * n));
         }
